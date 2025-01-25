c9d8fbbf02837fe1e252147b58f4b8997c993309
Emily Ha <em1lyha2002@gmail.com>
2025-01-25 20:28:52 +0100

    saving

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const dotenv = require('dotenv');
-
-dotenv.config();
+import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Backend is running');
+    res.send('Backend is running!');
 });
 
 app.listen(PORT, () => {

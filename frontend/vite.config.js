0257f53932e0409a0254cc2a2cb606999d5248ea
Emily Ha <em1lyha2002@gmail.com>
2025-02-01 20:15:44 +0100

    config files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Explicitly load .env files from the 'frontend' directory
-  const env = loadEnv(mode, './frontend', '');
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
     define: {
-      // Pass environment variables explicitly if needed
-      'process.env': env,
+      'import.meta.env': env, // Ensure env variables are passed correctly
     },
   };
 });
